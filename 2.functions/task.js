3abd992033b7f27e5ccceff3c58f94735a66aa98
wlistertula <tulaa478@gmail.com>
2025-03-06 22:21:17 +0300

    first function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,1 +1,25 @@
+function getArrayParams(...arr) {
+  if (arr.length === 0) {
+    return {};
+  }
+   let min = arr[0];
+   let max = arr[0];
+   let sum = 0;
 
+   for (let i = 0; i < arr.length; i++) {
+       const num = arr[i];
+
+       if (num < min) {
+           min = num;
+       }
+       if (num > max) {
+           max = num;
+       }
+
+  
+       sum += num;
+   }
+
+  const avg = +(sum / arr.length).toFixed(2);
+  return { min: min, max: max, avg: avg };
+}
