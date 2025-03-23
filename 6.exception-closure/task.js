9ea351b991912dad10d52bb4383e961af939359a
wlistertula <tulaa478@gmail.com>
2025-03-23 16:53:02 +0300

    исключения

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value); 
+    if (isNaN(parsedValue)) { 
+        throw new Error("Невалидное значение"); 
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+   
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2; 
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)); 
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c); // Пытаемся создать треугольник
+    } catch (error) {
+        // Возвращаем объект с геттерами, если треугольник не существует
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
